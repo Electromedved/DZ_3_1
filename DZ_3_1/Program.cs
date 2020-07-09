c9030d9b236a22ba64e9c61e15f43d85dd963055
Виталий Анатольевич <pigcontrol@gmail.com>
2020-07-09 21:42:29 +0300

    Add project files.

---
+++ DZ_3_1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ_3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result;
+
+            for (int i = 1; i <= 9; i++)
+            {
+                result = 7 * i;
+                Console.WriteLine($"7 * {i} = {result}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
